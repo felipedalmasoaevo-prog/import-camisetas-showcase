9bc254d4e98e3c7733f02662562defdf1128ff6a
felipedalmasoaevo-prog <felipedalmaso.aevo@gmail.com>
2026-03-24 17:27:49 -0300

    Update ClientsSection.tsx

--- src/components/ClientsSection.tsx
+++ src/components/ClientsSection.tsx
@@ -2,6 +2,7 @@ const CLIENTS = [
   { name: "Nestlé", logo: "/clients/Nestle_textlogo_blue.svg" },
   { name: "Vale", logo: "/clients/ValeLogoUnit.png" },
   { name: "40 Sabores", logo: "/clients/40sab.png" },
+  { name: "China in Box", logo: "china-in-box.png" },
 ];
 
 const ClientsSection = () => {
